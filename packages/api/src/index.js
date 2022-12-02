bee622c6a0d8da7542f0e5fcebb5f803f8b1fa1c
qxb3 <qxbthree@gmail.com>
2022-12-02 19:36:00 +0800

    [api] feat: added logger using pino-pretty

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -3,8 +3,18 @@ require('dotenv/config')
 const { build } = require('./app')
 
 async function start() {
-  const port = process.env.PORT ?? 5000
-  const app = build()
+  const port = process.env.PORT || 5000
+  const app = build({
+    logger: {
+      transport: process.env.NODE_ENV === 'development' ? {
+        target: 'pino-pretty',
+        options: {
+          translateTime: 'HH:MM:ss Z',
+          ignore: 'pid,hostname'
+        }
+      } : null
+    }
+  })
 
   try {
     await app.listen({ port })
--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,5 +1,3 @@
 module.exports = (app, _options, done) => {
-  app.get('/', (_, reply)=> reply.send('ok'))
-
   done()
 }
