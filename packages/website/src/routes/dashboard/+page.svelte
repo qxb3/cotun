21c0e601e2456e71c47f2e8059e81d7489a57874
qxb3 <qxbthree@gmail.com>
2022-12-11 18:42:32 +0800

    [website] feat: added dashboard route

---
+++ packages/website/src/routes/dashboard/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  const namespaces = [
+    {
+      name: 'views',
+      count: 16
+    }
+  ]
+</script>
+
+<svelte:head>
+  <title>Dashboard</title>
+</svelte:head>
+
+<div class="container p-3">
+  <h1 class="text-3xl mt-3">Dashboard</h1>
+
+  <div class="mt-12">
+    <div class="flex justify-between place-items-center">
+      <h1 class="text-2xl">Namespaces:</h1>
+      <button class="button bg-blue-400 text-white border-blue-300">New +</button>
+    </div>
+
+    <div class="mt-6">
+      {#each namespaces as { name, count }}
+        <div class="flex justify-between border rounded-md my-2 p-2">
+          <div>
+            <h1>Name: {name}</h1>
+            <p>Count: {count}</p>
+          </div>
+
+          <div>
+            <button class="button">Edit</button>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
