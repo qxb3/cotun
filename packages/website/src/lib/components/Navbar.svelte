80c7f3cd1c5261f827ab937e41e17d6677829bbf
qxb3 <qxbthree@gmail.com>
2022-12-11 13:57:13 +0800

    [website] feat: finished home page

---
+++ packages/website/src/lib/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+  let isOpen = false
+
+  function toggleNav() {
+    isOpen = !isOpen
+  }
+</script>
+
+<nav class="shadow">
+  <div class="container flex align-center justify-between">
+    <div class="p-2">
+      <img class="w-11 h-11" src="/cotun.png" alt="Brand Logo">
+    </div>
+
+    <div class="grid place-items-center p-3">
+      <ul class="flex gap-8">
+        <li>
+          <a href="/docs">Docs</a>
+        </li>
+
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- packages/website/src/routes/+page.svelte
+++ packages/website/src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+  import Navbar from '$lib/components/Navbar.svelte'
+</script>
+
+<Navbar />
+
+<div class="container">
+  <header class="mt-16 text-center">
+    <h1 class="text-6xl md:text-8xl">cotun</h1>
+    <p class="md:text-3xl">A counter as a service</p>
+  </header>
+
+  <div class="mt-16 text-center">
+    <a class="button" href="/login">Get Started</a>
+  </div>
+</div>
