b18a34949509d7bce44f14a7dc51ad9fb2060610
qxb3 <qxbthree@gmail.com>
2022-12-11 14:11:06 +0800

    [website] fix: added +error.svelte

---
+++ packages/website/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores'
+</script>
+
+<div class="mt-16 text-center">
+  <h1 class="text-8xl">{$page.status}</h1>
+  <p class="text-5xl">{$page.error.message}</p>
+</div>
