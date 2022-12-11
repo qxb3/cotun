c49d9fd1b9d9c28b4bdbba0a5b44292b04a02816
qxb3 <qxbthree@gmail.com>
2022-12-11 19:04:17 +0800

    [website] fix(Navbar.svelte): removed toggleNav function

--- packages/website/src/lib/components/Navbar.svelte
+++ packages/website/src/lib/components/Navbar.svelte
@@ -1,13 +1,8 @@
 <script>
-  let isOpen = false
-
-  function toggleNav() {
-    isOpen = !isOpen
-  }
 </script>
 
 <nav class="shadow">
-  <div class="container flex justify-between">
+  <div class="container flex justify-between items-center">
     <a href="/" class="p-2">
       <img class="w-11 h-11" src="/cotun.png" alt="Brand Logo">
     </a>
