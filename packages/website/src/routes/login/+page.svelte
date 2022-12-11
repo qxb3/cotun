4d050eef4a459932472ae4b0afb6b303fb45e587
qxb3 <qxbthree@gmail.com>
2022-12-11 14:53:32 +0800

    [website] feat: added login route

---
+++ packages/website/src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  function login() {
+    console.log('Login')
+  }
+</script>
+
+<div class="container p-3">
+  <h1 class="text-3xl text-center my-12">Login</h1>
+
+  <form on:submit|preventDefault={login} class="grid place-items-center w-full">
+    <div>
+      <div class="space-y-3">
+        <input class="w-full" type="text" placeholder="Username">
+        <input class="w-full" type="password" placeholder="Password">
+      </div>
+
+      <button class="button mt-3 w-full" type="submit">Login</button>
+    </div>
+  </form>
+</div>
