fc8692409f60ce7b8c225e4e1c9b092c72430b8d
qxb3 <qxbthree@gmail.com>
2022-12-11 15:19:47 +0800

    [website] feat: added register route

--- packages/website/src/routes/login/+page.svelte
+++ packages/website/src/routes/login/+page.svelte
@@ -8,7 +8,7 @@
   <h1 class="text-3xl text-center my-12">Login</h1>
 
   <form on:submit|preventDefault={login} class="grid place-items-center w-full">
-    <div>
+    <div class="max-w-lg min-w-lg">
       <div class="space-y-3">
         <input class="w-full" type="text" placeholder="Username">
         <input class="w-full" type="password" placeholder="Password">
---
+++ packages/website/src/routes/register/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  function register() {
+    console.log('Register')
+  }
+</script>
+
+<div class="container p-3">
+  <h1 class="text-3xl text-center my-12">Register</h1>
+
+  <form on:submit|preventDefault={register} class="grid place-items-center w-full">
+    <div class="max-w-lg min-w-lg">
+      <div class="space-y-3">
+        <input class="w-full" type="text" placeholder="Email">
+        <input class="w-full" type="text" placeholder="Username">
+        <input class="w-full" type="password" placeholder="Password">
+      </div>
+
+      <button class="button mt-3 w-full" type="submit">Register</button>
+    </div>
+  </form>
+
+  <div class="text-center mt-12">
+    <p>
+      Already have an account?
+      <a class="underline" href="/login">Sign In!</a>
+    </p>
+  </div>
+</div>
