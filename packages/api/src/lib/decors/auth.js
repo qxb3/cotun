54aa3b60a1d6a7af1b6a55a86a4c53cf55b79582
qxb3 <qxbthree@gmail.com>
2022-12-12 11:33:26 +0800

    [api] feat(decors): added validateApiKey

---
+++ packages/api/src/lib/decors/auth.js
@@ -0,0 +1,17 @@
+const Users = require('#models/users.js')
+
+const validatedApiKey = async (req, reply, done) => {
+  const { authorization } = req.headers
+
+  const user = await Users.findOne({ apiToken: authorization })
+  if (!user) {
+    return reply.unauthorized('Invalid api key')
+  }
+
+  req.user
+  done()
+}
+
+module.exports = {
+  validatedApiKey
+}
