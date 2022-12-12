976b327c5864eb418e0d44a1f18b2aa236761685
qxb3 <qxbthree@gmail.com>
2022-12-12 21:58:45 +0800

    [api] feat(routes): added register route

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,6 +1,10 @@
+const register = require('#routes/user/register.js')
+
 const count = require('#routes/count.js')
 
 module.exports = (app, _options, done) => {
+  app.register(register, { prefix: '/user' })
+
   app.register(count)
 
   done()
