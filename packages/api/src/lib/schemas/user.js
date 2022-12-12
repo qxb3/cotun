eea263ff6bba4571903393b3a9f3efdff173122c
qxb3 <qxbthree@gmail.com>
2022-12-12 22:02:01 +0800

    [api] feat(schemas): finished user schema

--- packages/api/src/lib/schemas/user.js
+++ packages/api/src/lib/schemas/user.js
@@ -2,5 +2,16 @@ module.exports = {
   $id: 'user',
   type: 'object',
   properties: {
+    id: { type: 'string' },
+    username: { type: 'string' },
+    password: { type: 'string' },
+    apiKey: { type: 'string' },
+    counters: {
+      type: 'array',
+      items: {
+        name: { type: 'string' },
+        count: { type: 'integer' }
+      }
+    }
   }
 }
