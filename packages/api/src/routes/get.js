a21109c8a9d66f6263d4cd66b4b1497e9cea8d3e
qxb3 <qxbthree@gmail.com>
2022-12-18 18:24:11 +0800

    fix(get route): fix schema

--- packages/api/src/routes/get.js
+++ packages/api/src/routes/get.js
@@ -10,16 +10,18 @@ module.exports = (app, _options, done) => {
         properties: {
           name: { type: 'string', minLength: 3 }
         }
+      },
+      response: {
+        200: {
+          anyOf: [
+            { $ref: 'counter' },
+            {
+              type: 'array',
+              items: { $ref: 'counter' }
+            }
+          ]
+        }
       }
-      // response: {
-      //   200: {
-      //     type: ['object', 'array'],
-      //     enum: [
-      //       { $ref: 'counter' },
-      //       { items: { $ref: 'counter' } }
-      //     ]
-      //   }
-      // }
     },
     preHandler: (req, reply, done) => app.auth.validatedApiKey(req, reply, done),
     handler: async (req, reply) => {
@@ -50,25 +52,14 @@ module.exports = (app, _options, done) => {
       reply.badRequest(`There is no counter called ${name}`)
     }
 
-    // TODO: Fix this after i figure out the schema thingy
-    reply.send({
-      name: counter.name,
-      count: counter.count
-    })
+    reply.send(counter)
   }
 
   async function getAll(req, reply) {
     const { user } = req
 
     const counters = await Counters.find({ userId: user._id })
-
-    // TODO: Fix this after i figure out the schema thingy
-    const filteredCounters = counters.map(v => ({
-      name: v.name,
-      count: v.count
-    }))
-
-    reply.send(filteredCounters)
+    reply.send(counters)
   }
 
   done()
