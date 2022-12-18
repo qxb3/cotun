1e767408d7b1fbf2f298632f228c2d9dee1b0f98
qxb3 <qxbthree@gmail.com>
2022-12-18 16:49:13 +0800

    feat(schemas): added counter schema

---
+++ packages/api/src/lib/schemas/counter.js
@@ -0,0 +1,9 @@
+module.exports = {
+  $id: 'counter',
+  type: 'object',
+  properties: {
+    userId: { type: 'string' },
+    name: { type: 'string' },
+    count: { type: 'integer' }
+  }
+}
--- packages/api/src/lib/schemas/index.js
+++ packages/api/src/lib/schemas/index.js
@@ -1,9 +1,11 @@
 const plugin = require('fastify-plugin')
 
 const user = require('#schemas/user.js')
+const counter = require('#schemas/counter.js')
 
 module.exports = plugin((app, _options, done) => {
   app.addSchema(user)
+  app.addSchema(counter)
 
   done()
 })
