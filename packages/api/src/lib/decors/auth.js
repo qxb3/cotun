c1a9f1a8ab9dac21c8d5d04b4fb2874ef30b7540
qxb3 <qxbthree@gmail.com>
2022-12-18 16:48:29 +0800

    fix(auth decors): fix validate api token

--- packages/api/src/lib/decors/auth.js
+++ packages/api/src/lib/decors/auth.js
@@ -1,15 +1,17 @@
 const Users = require('#models/users.js')
 
-const validatedApiKey = async (req, reply, done) => {
+const validatedApiKey = (req, reply, done) => {
   const { authorization } = req.headers
 
-  const user = await Users.findOne({ apiToken: authorization })
-  if (!user) {
-    return reply.unauthorized('Invalid api key')
-  }
+  Users.findOne({ apiToken: authorization })
+    .then(user => {
+      if (!user) {
+        return reply.unauthorized('Invalid api key')
+      }
 
-  req.user = user
-  done()
+      req.user = user
+      done()
+    })
 }
 
 module.exports = {
