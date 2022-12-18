235941449621757829a3f52dfc6ffb510ac3ae75
qxb3 <qxbthree@gmail.com>
2022-12-18 19:06:02 +0800

    [api] ref(user schema): removed id and counters

--- packages/api/src/lib/schemas/user.js
+++ packages/api/src/lib/schemas/user.js
@@ -2,16 +2,8 @@ module.exports = {
   $id: 'user',
   type: 'object',
   properties: {
-    id: { type: 'string' },
     username: { type: 'string' },
     password: { type: 'string' },
-    apiKey: { type: 'string' },
-    counters: {
-      type: 'array',
-      items: {
-        name: { type: 'string' },
-        count: { type: 'integer' }
-      }
-    }
+    apiKey: { type: 'string' }
   }
 }
