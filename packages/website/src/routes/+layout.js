b44fdbb5c2ca3a36d4ab1c49bce2e437d04af6e6
qxb3 <qxbthree@gmail.com>
2022-12-21 13:37:55 +0800

    [website] feat: validate authentication

--- packages/website/src/routes/+layout.js
+++ packages/website/src/routes/+layout.js
@@ -1,4 +1,27 @@
+import { PUBLIC_API_URL } from '$env/static/public'
+import { goto } from '$app/navigation'
+import userStore from '$lib/stores/user.js'
+
 export const load = async ({ url }) => {
+	if (url.pathname === '/dashboard') {
+		const res = await fetch(`${PUBLIC_API_URL}/_user/get`, {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		const data = await res.json()
+
+		if (res.status !== 200) {
+			return goto('/login')
+		}
+
+		const { username, apiKey } = data
+		userStore.set({
+			username,
+			apiKey
+		})
+	}
+
 	return {
 		pathname: url.pathname
 	}
